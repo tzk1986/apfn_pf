868a6284f9a54d857aaacbd3f7cc53645a28694c
tzk1986 <tzk1986@gmail.com>
2024-11-25 14:38:52 +0800

    csv参数化,队列queue方法使用

--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,26 @@
-# from locust import HttpUser, task
-
-# class HelloWorldUser(HttpUser):
-#     @task
-#     def hello_world(self):
-#         self.client.get("/vip/vip/list")
-#         self.client.get("/vip/face/list")
-        
-        
 import time
 from locust import HttpUser, constant, constant_pacing, constant_throughput, task, between
+import queue
+from tools.loader import read_csv_file
 
-class QuickstartUser(HttpUser):
+
+class QuickstartUser(HttpUser) :
+    
+    host = 'http://10.50.11.120:9001'
+    
     # wait_time = between(1, 5)
     wait_time = constant_throughput(50)
     # wait_time = constant_pacing(1)
     # wait_time = constant(1)
+    
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.q = queue.Queue()  # 初始化一个队列
+    
+    # def on_start(self):
+    #     csv_list = read_csv_file("data.csv")
+    #     for i in csv_list:
+    #         self.q.put(i)  # 将数据添加到队列中
 
     @task
     def hello_world(self):
---
+++ tools/loader.py
@@ -0,0 +1,17 @@
+import csv
+from typing import Text
+
+
+def read_csv_file(csv_file: Text):
+    """
+    读取CSV文件并将其转换为列表
+
+    :param csv_file: CSV文件的路径
+    :return: 一个列表的列表，其中每个内部列表代表CSV文件中的一行
+    """
+    csv_content_list = []
+    with open(csv_file, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            csv_content_list.append(row)
+    return csv_content_list
